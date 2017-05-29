ee9341d93ce66e94dd584838156b4bff6618a863
Ulrich Schmidt <u.schmidt@velian.de>
2017-05-29 09:12:30 +0200

    Link öffnet in neuem Fenster

--- src/content/modules/google_pagespeed_insights/controllers/google_pagespeed_controller.php
+++ src/content/modules/google_pagespeed_insights/controllers/google_pagespeed_controller.php
@@ -2,7 +2,7 @@
 class PageSpeedController extends Controller {
 	public function adminMenuEntriesFilter($entries) {
 		$url = "https://developers.google.com/speed/pagespeed/insights/?hl=" . urlencode ( getSystemLanguage () ) . "&url=" . urlencode ( get_protocol_and_domain () );
-		$entries [count ( $entries ) - 1] = new MenuEntry ( "PageSpeed Insights", $url, "pagespeed" );
+		$entries [count ( $entries ) - 1] = new MenuEntry ( "PageSpeed Insights", $url, "pagespeed", null, array (), true );
 		$entries [] = new MenuEntry ( get_translation ( "logout" ), "?action=destroy", "destroy" );
 		return $entries;
 	}
