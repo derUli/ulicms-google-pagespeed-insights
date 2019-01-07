b89256b0d4831d488f549736e21755a0e54e576b
Ulrich Schmidt <u.schmidt@velian.de>
2019-01-07 09:01:39 +0100

    update compatible from, update min UliCMSversion g

--- src/content/modules/google_pagespeed_insights/controllers/google_pagespeed_controller.php
+++ src/content/modules/google_pagespeed_insights/controllers/google_pagespeed_controller.php
@@ -7,7 +7,10 @@ class PageSpeedController extends Controller
     {
         $url = "https://developers.google.com/speed/pagespeed/insights/?hl=" . urlencode(getSystemLanguage()) . "&url=" . urlencode(get_protocol_and_domain());
         $entries[count($entries) - 1] = new MenuEntry('<i class="fas fa-chart-line"></i> PageSpeed Insights', $url, "pagespeed", null, array(), true);
-        $entries[] = new MenuEntry('<i class="fa fa-sign-out-alt"></i> ' . get_translation("logout"), "?action=destroy", "destroy");
+       
+        $logoutUrl = ModuleHelper::buildMethodCallUrl(SessionManager::class, "logout");
+        $entries[] = new MenuEntry('<i class="fa fa-sign-out-alt"></i> ' . get_translation("logout"), $logoutUrl, "logout");
+
         return $entries;
     }
 }
